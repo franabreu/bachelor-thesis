73803660630357b1473684be20bc638ee24cd9b3
Fran <fraabrvaz@alum.us.es>
2020-04-13 20:27:40 +0200

    Cities and countries config for database population

---
+++ resources/config.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyA8MmfBiJME8JOxiLfg19eKjGz4c1SeLOY",
+    authDomain: "tfgdb-6df87.firebaseapp.com",
+    databaseURL: "https://tfgdb-6df87.firebaseio.com",
+    projectId: "tfgdb-6df87",
+    storageBucket: "tfgdb-6df87.appspot.com",
+    messagingSenderId: "193755658498",
+    appId: "1:193755658498:web:c0575756e33135bb732a1b"
+};
+
+module.exports = firebaseConfig;
